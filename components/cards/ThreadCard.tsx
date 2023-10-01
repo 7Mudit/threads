5cfbc7e2c42cfe1dfeacaf46e3e56bffe5805f7a
Mudit Kapoor <muditk441@gmail.com>
2023-10-01 18:47:25 +0530

    thread page and commenting working

--- components/cards/ThreadCard.tsx
+++ components/cards/ThreadCard.tsx
@@ -37,7 +37,11 @@ function ThreadCard({
   isComment,
 }: Props) {
   return (
-    <article className="flex w-full flex-col rounded-xl bg-dark-2 p-7">
+    <article
+      className={`flex w-full flex-col rounded-xl  ${
+        isComment ? "px-0 xs:px-7" : "bg-dark-2 p-7"
+      }`}
+    >
       <div className="flex items-start justify-between">
         <div className="flex w-full flex-1 flex-row gap-4">
           <div className="flex flex-col items-center">
@@ -92,9 +96,11 @@ function ThreadCard({
                   className="cursor-pointer object-contain"
                 />
               </div>
-              {isComment && comments.length > 0 &&(
+              {isComment && comments.length > 0 && (
                 <Link href={`/thread/${id}`}>
-                    <p className="mt-1 text-subtle-medium text-gray-1">{comments.length} replies</p>
+                  <p className="mt-1 text-subtle-medium text-gray-1">
+                    {comments.length} repl{comments.length > 1 ? "ies" : "y"}
+                  </p>
                 </Link>
               )}
             </div>
